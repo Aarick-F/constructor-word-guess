3d0d86741e3aa87c9f7b57d636782b568e2a077e
Aarick-F <zeflyndesign@gmail.com>
2018-04-27 23:10:13 -0400

    fixed bugs and ironed out the exit menus

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ function init() {
 
 function playGame() {
   pickedWord = "";
-  guesses = 9;
+  guesses = 15;
   if(pickedWords.length < wordBank.length) {
     pickedWord = getWord();
   } else {
     // WIN CONDITION
     console.log("You know a lot about your celestial neighborhood. Cheers!");
+    continuePrompt();
   }
   if(pickedWord) {
     word = new Word(pickedWord);
@@ -44,7 +45,7 @@ function getWord() {
     pickedWords.push(randomWord);
     return randomWord;
   } else {
-    getWord();
+    return getWord();
   }
 }
 
@@ -54,20 +55,46 @@ function makeGuess() {
     {
       name: "guessedLetter",
       message: word.update() + 
-              "\nGuess a letter!"
+              "\nGuess a letter!" +
+              "\nGuesses Left: " + guesses
     }
-  ]).then(data => {
+  ])
+  .then(data => {
     word.letters.forEach(letter => {
       letter.checkLetter(data.guessedLetter);
       checker.push(letter.getCharacter());
     });
     if(guesses > 0 && checker.indexOf("_") !== -1) {
-      makeGuess();
+      guesses--;
+      if(guesses === 0) {
+        console.log("YOU RAN OUT OF GUESSES! GAME OVER.");
+        continuePrompt();
+      } else {
+        makeGuess();
+      }
     } else {
       console.log("CONGRATULATIONS! YOU GOT THE WORD!");
       console.log(word.update());
       playGame();
     }
+  });
+}
+
+function continuePrompt() {
+  inquirer.prompt([
+      {
+        name: "continue",
+        type: "list",
+        message: "Would you like to play again?",
+        choices: ["Yes", "No"]
+      }
+    ])
+  .then(data => {
+      if(data.continue === "Yes") {
+        init();
+      } else {
+        console.log("Thanks for playing!");
+      }
   });
 }
 
